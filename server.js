74379770e46f9c19f8ff87a43dbe9c147636e988
jtiagotoledo <jtiagotoledo@gmail.com>
2025-10-19 16:05:00 -0300

    criado modelo para tarefas

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import Todo from './models/Todo'
+
+dotenv.config()
 const port = 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Servidor ok');
 })
 
-app.post('/aluno',(req,res)=>{
-    const aluno = req.body;
-    res.json({mensagem:'aluno cadastrado',dados:aluno});
-})
+app.post('/tarefa', async (req, res) => {
+    try {
+        const { titulo, concluida } = req.body;
 
-app.listen(port, ()=>{
-    console.log(`Servidor rodando na porta ${port}`);
-})
+        const novaTarefa = new Todo({ titulo, concluida });
+        const salva = novaTarefa.save();
+
+        res.json({ mensagem: 'tarefa cadastrada', dados: salva });
+    } catch (err) {
+        res.status(500).json({ erro: 'Erro ao salvar tarefa', detalhes: err.message });
+    }
+});
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        })
+
+    }).catch((err) => {
+        console.log('Erro ao conectar ao MongoDB', err);
+    })
+
+
 
